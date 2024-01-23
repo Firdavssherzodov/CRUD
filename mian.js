4e0f716c910fe89a1cfabc33243d0d7d69b4e9fb
firdavs <frontendhtml1234@gmail.com>
2024-01-23 15:05:54 +0500

    s

--- mian.js
+++ mian.js
@@ -9,50 +9,61 @@ const app = Vue.createApp({
           job: "Fronted development",
         },
         {
-          id: 1,
+          id: 2,
           name: "Lutfilla",
           email: "Lutfilla@gmail.com",
           job: "Mobile development",
         },
         {
-          id: 1,
+          id: 3,
           name: "Shomirza",
           email: "Shomirza@gmail.com",
           job: "Node Js ",
         },
         {
-          id: 1,
+          id: 4,
           name: "Qulpiddin",
           email: "Qulpiddin@gmail.com",
           job: "G'irt bekorchi",
         },
       ],
+      id: "",
       name: "",
       email: "",
       job: "",
       errorHnadle: "",
       error: false,
+      handleUserid: null,
     };
   },
 
   methods: {
-
-    Cancellation(){
-
-     return this.error = false;
+    Cancellation() {
+      return (this.error = false);
     },
 
     Delete(index) {
       this.data.splice(index, 1);
     },
     addUser() {
-      if (this.name == "" && this.email == "" && this.job == "") {
+      if (this.handleUserid != null) {
+        let findById = this.data.find((user) => user.id == this.handleUserid);
+        findById.name = this.name;
+        findById.email = this.email;
+        findById.job = this.job;
+        this.handleUserid = null;
+        this.name = "";
+        this.email = "";
+        this.job = "";
+  
+      } else if (this.name == "" && this.email == "" && this.job == "") {
         this.error = true;
         this.errorHnadle = "Please enter empty Name and Email and Job";
         return;
-      }
+      } else{
 
       let newUser = {
+        id: this.data.length + 1,
         name: this.name,
         email: this.email,
         job: this.job,
@@ -63,15 +74,16 @@ const app = Vue.createApp({
       this.name = "";
       this.email = "";
       this.job = "";
+
+    }
     },
 
-    Update(index) {
-      this.data.filters((list) => {
-        // this.name = list.name,
-        // this.email = list.email,
-        // this.job = list.job
-        console.log(list);
-      });
+    Update(id) {
+      let findById = this.data.find((user) => user.id == id);
+      (this.name = findById.name),
+        (this.email = findById.email),
+        (this.job = findById.job);
+      this.handleUserid = id;
     },
   },
 
